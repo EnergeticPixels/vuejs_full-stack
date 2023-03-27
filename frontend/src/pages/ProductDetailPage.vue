70c3cac4f1180e8695953c8c8cdce3682a2f746b
Anthony Jackman <energeticpixels@gmail.com>
2023-03-27 14:29:53 -0500

    Add NotFoundPage to details page

--- frontend/src/pages/ProductDetailPage.vue
+++ frontend/src/pages/ProductDetailPage.vue
@@ -1,22 +1,32 @@
 <template>
-  <div class='img-wrap'>
-    <img v-bind:src='product.imageName' />
+  <div v-if='product'>
+    <div class='img-wrap'>
+      <img v-bind:src='product.imageName' />
+    </div>
+    <div class='product-details'>
+      <h1>{{ product.name }}</h1>
+      <h3 class='price'>{{ product.price }}</h3>
+      <button class='add-to-cart'>Add to Cart</button>
+    </div>
   </div>
-  <div class='product-details'>
-    <h1>{{ product.name }}</h1>
-    <h3 class='price'>{{ product.price }}</h3>
-    <button class='add-to-cart'>Add to Cart</button>
+  <div v-if='!product'>
+    <NotFoundPage />
   </div>
+
 </template>
 
 <script>
 import { products } from '../temp-data';
+import NotFoundPage from './NotFoundPage';
 export default {
   name: "ProductDetail",
   data() {
     return {
       product: products.find(product => product.id === this.$route.params.productId)
     }
+  },
+  components: {
+    NotFoundPage
   }
 }
 </script>
