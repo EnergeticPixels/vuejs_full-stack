9dd5a3b49e36d87768a77311f3a3de2c82c84527
Anthony Jackman <energeticpixels@gmail.com>
2023-03-27 15:38:04 -0500

    REFACTOR products page
    created ProductsList into a component then carry data in vue props

---
+++ frontend/src/components/ProductsList.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class='grid-wrap'>
+    <div v-for="product in products" v-bind:key="product.id" class='product-item'>
+      <img v-bind:src='product.imageName' />
+      <h3 class='product-name'>{{ product.name }}</h3>
+      <p class='product-price'>{{ product.price }}</p>
+      <router-link :to="'/products/' + product.id">
+        <button>View Details</button>
+      </router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ProductsList',
+  props: ['products']
+}
+</script>
+
+<style scoped>
+
+</style>
