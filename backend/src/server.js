4b65afa6b0e7a3754ad3d464718dc8e635d9cd2d
Anthony Jackman <energeticpixels@gmail.com>
2023-06-20 12:51:05 -0500

    REFACTOR add item to user cart
    Adds database connectivity

--- backend/src/server.js
+++ backend/src/server.js
@@ -62,10 +62,15 @@ async function start() {
     res.json(product);
   });
 
-  app.post('/cart', (req, res) => {
+  app.post('/users/:userId/cart', async (req, res) => {
+    const userId = req.params.userId;
     const productId = req.body.id;
-    cartItems.push(productId);
-    const populatedCart = populateCartId(cartItems);
+
+    await db.collection('users').updateOne({ id: userId }, { 
+      $addToSet: { cartItems: productId }
+    });
+    const user = await db.collection('users').findOne({ id: req.params.userId });
+    const populatedCart = await populateCartId(user.cartItems);
     res.json(populatedCart);
   });
 
